acd2d6e797411f5a39be90f065b750c41a9dc5e6
Pedro Felipe <73067899+NeptUser@users.noreply.github.com>
2022-11-23 20:18:04 -0300

    Add files via upload

---
+++ sketch_sep14a/sketch_sep14a.ino
@@ -0,0 +1,40 @@
+//Avenida 6
+
+
+//FUNÇÕES PRINCIPAIS
+void garra(){
+ //função responsavel pela abertura e fechamento da garra
+}
+
+void atuador(){
+  //função resposável por gerar movimento no atuador
+  //fazer o atuador rodar com motor dc e com precisão
+  //avaliar possibilidade de criar função auxiliar
+}
+void quantidade(){
+  //vai identificar a quantidade  a partir da interface que se comunica com o us~ário
+  //vai chamar a função do atuador e da garra
+}
+void limpador(){
+  //remove todos os blocos para o depósito e deixa o sistema pronto para um novo uso
+}
+
+//FUNÇÕES AUXILIARES
+
+bool estadodagarra(){
+  //função auxiliar para definir se a garra está aberta pou fechada
+  //retorna true para abrir e false para fechar
+}
+
+
+
+
+//FUNÇÕES DO ARDUINO
+
+void setup{
+  
+}
+
+void loop{
+  
+}
