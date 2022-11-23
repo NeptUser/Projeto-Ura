630b4478d44cde1c740721d5820c8717a9f82229
Pedro Felipe <felipepedro752@gmail.com>
2022-11-23 00:07:15 -0300

    Create sketch_nov23a.ino

---
+++ sketch_nov23a/sketch_nov23a.ino
@@ -0,0 +1,13 @@
+//fazer garra para poegar bolbos que representam quantidades numericas e colocar-los em espaçosdesiginados para ensiar as crianças sobre dezena centana unidad e milhar
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
